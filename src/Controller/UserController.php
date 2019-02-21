9e1945ddd0072c5b7e8a646e3c9a4db62fb9bcbe
micou1830 <38948344+micou1830@users.noreply.github.com>
2019-02-21 16:19:16 +0100

    Crud (#34)
    * crud user
    
    * Crud User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -72,4 +72,162 @@ class UserController extends AbstractController
 
         return new Response($emailService->sendEmail($userMailData));
     }
+
+    // CRUD
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("/add", name="user_add", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function add(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $content = $request->getContent();
+
+        $data = json_decode($content, true);
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw new Error('No User found');
+        }
+
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        try {
+            $form->submit($data);
+        } catch (\Exception $e) {
+            throw new Exception($e->getMessage(), 500);
+        }
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $user->setUser($user);
+            $em->persist($user);
+            $em->flush();
+
+        }
+        $data = $this->get('serializer')->serialize($user, 'json');
+
+        return new JsonResponse($data, 200, [], true);
+
+    }
+
+    /**
+     * @Route("/show", name="user_show", methods={"GET"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getUser(Request $request, UserRepository $userRepository)
+    {
+        $content = $request->getContent();
+
+        $data = json_decode($content, true);
+
+        $user = $userRepository->findOneBy($data);
+
+        $connectedUser = $this->getUser();
+        $userOfUser = $user->getUser();
+
+        if ($connectedUser !== $userOfUser) {
+            throw new Error('operation not allowed');
+        }
+
+        $user = [
+            'nom' => $user->getNom(),
+            'prenom' => $user->getPrenom(),
+            'email' => $user->getEmail(),
+            'telephone' => $user->getTelephone(),
+            'nom_entreprise' => $user->getNomEntreprise(),
+            'adresse' => $user->getAdresse(),
+            'code_postal' => $user->getCodePostal(),
+            'ville' => $user->getVille(),
+            'logo' => $user->getLogo(),
+            'password' => $user->getPassword(),
+            'social' => $user->getSocial(),
+            'site_web' => $user->getSiteWeb(),
+            'slug' => $user->getSlug()
+        ];
+
+        $data = $this->get('serializer')->serialize($user, 'json');
+
+        return new JsonResponse($data, 200, [], true);
+    }
+
+
+    /**
+     * @Route("/delete", name="user_delete", methods={"DELETE"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function deleteUser(Request $request, UserRepository $userRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $content = $request->getContent();
+
+        $myData = json_decode($content, true);
+        $id = $myData['id'];
+        $user = $userRepository->findOneBy(['id' => $id]);
+
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+
+        $data = $this->get('serializer')->serialize('user deleted', 'json');
+
+        return new JsonResponse($data, 200, [], true);
+    }
+
+
+    /**
+     * @Route("/update/{userId}", name="user_update", methods={"PUT"}, requirements={"userId"="\d+"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateUser(Request $request, UserRepository $userRepository, $userId)
+    {
+        $content = $request->getContent();
+        $entityManager = $this->getDoctrine()->getManager();
+        $myData = json_decode($content, true);
+
+        $users = $userRepository->find($userId);
+        $user = $user->getUser();
+        $currentUser = $this->getUser();
+
+        if ($users !== $currentUser) {
+            throw new Error('update is refused');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
+        try {
+            $form->submit($myData);
+        } catch (\Exception $e) {
+            throw new Error('error');
+        }
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->flush();
+        }
+
+        $data = $this->get('serializer')->serialize($user, 'json');
+
+        return new JsonResponse($data, 200, [], true);
+
+    }
+
+
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('nom_entreprise')
+            ->add('logo')
+            ->add('adresse')
+            ->add('code_postal')
+            ->add('ville')
+            ->add('site_web')
+            ->add('social')
+            ->add('telephone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
